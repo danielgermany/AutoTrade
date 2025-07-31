0788edb87c1bf7f05122e9b92f5e9e05a01fc3d4
danielgermany <dgermany333@gmail.com>
2025-07-31 12:50:30 -0400

    Removing old code and changing logic to check for support and resistance

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 import yfinance as yf
 import pandas as pd
 
-# Download 1-hour EUR/USD data
-dataF = yf.download("EURUSD=X", start="2024-10-04", end="2025-05-10", interval="1h")
+# Download data
+dataF = yf.download("MNQ=F", start="2024-10-04", end="2025-05-10", interval="1h")
+dataF.dropna(inplace=True)
 
-def signal_generator(df):
-    open_ = float(df['Open'].iloc[-1])
-    close = float(df['Close'].iloc[-1])
-    previous_open = float(df['Open'].iloc[-2])
-    previous_close = float(df['Close'].iloc[-2])
+#Support and Resistance
 
-    # Bearish Pattern
-    if close < previous_open and previous_open <= open_:
-        return 1
+#df1 is the dataframe used
+#candle is the index
 
-    # Bullish Pattern
-    elif open_ <= previous_close and close > previous_open:
-        return 2
+#Checks the high and lows of the candles from the dataframe
 
-    else:
-        return 0
+def support(df1, candle, candles_before, candles_after):
+    for i in range(1-candles_before+1, candle+1):
+        if df1.low[i]>df1.low[i - 1]:
+            return 0
+    for i in range(candle+1,candle+candles_after+1):
+        if df1.low[i]<df1.low[i - 1]:
+            return 0
+    return 1
 
-# Initialize signal list
-signal = [0]  # First row has no previous data
-
-# Loop from second row onward
-for i in range(1, len(dataF)):
-    df_slice = dataF.iloc[i-1:i+1]
-    signal.append(signal_generator(df_slice))
-
-dataF["signal"] = signal
-print(dataF.signal.value_counts())
+def resistance (df1, candle, candles_before, candles_after):
+    for i in range(candle-candles_before+1, candle+1):
+        if df1.high[i]<df1.high[i-1]:
+            return 0
+    for i in range(candle+1,candle+candles_after+1):
+        if df1.high[i]>df1.high[i-1]:
+            return 0
+    return 1
