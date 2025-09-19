bd6d43751a86da1f94fd7f80499a79d3238ef491
danielgermany <dgermany333@gmail.com>
2025-09-19 15:42:10 -0400

    Cleaning files

--- backtest.py
+++
@@ -1,8 +0,0 @@
-from backtesting import Backtest
-from db import df
-from Strat import strat
-
-bt = Backtest(df,strat, cash= 50_000)
-stat = bt.run()
-
-print(stat)
--- random_signals.py
+++
@@ -1,6 +0,0 @@
-# random_signals.py
-import numpy as np
-
-def gen_signals(close):
-    np.random.seed(42)  # For reproducibility
-    return np.random.choice([1, 2], size=len(close))
